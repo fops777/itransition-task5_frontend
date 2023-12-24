07e6cba4a95ec482200952103c8623c1d86abc02
fops777 <huzin.ayratik@gmail.com>
2023-12-24 18:00:10 +0500

    adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       // console.log(isFetching);
       async function getUsers() {
         let response = axios(process.env.REACT_APP_API_URL);
+        // let response = axios("http://localhost:5555/");
         const data = await response;
         setUsers([...users, ...data.data]);
         response.then(() => setIsFetching(false));
@@ -39,16 +40,65 @@ function App() {
     }
   }
 
-  const handleRangeChange = (event) => {
+  const handleSliderChange = (event) => {
     const newValue = event.target.value;
-    console.log(newValue);
     setValueSlider(newValue);
+
+    let arrWithErrors = users.map((user) => {
+      // Надо логику: иногда ретурнить user.name, иногда user.addres, иногда user.phone
+      return { ...user, name: addRandomError(user.name) };
+    });
+
+    setUsers(arrWithErrors);
   };
+  console.log(valueSlider);
+
+  // Генерация случайной ошибки в строке
+  function addRandomError(string) {
+    const errorType = Math.floor(Math.random() * 3);
+    let result = string;
+
+    for (let i = 0; i < 5; i++) {
+      // Выбираем случайный индекс, где будет находиться ошибка
+      const index = Math.floor(Math.random() * string.length);
+
+      switch (errorType) {
+        // Замена символа
+        case 0:
+          const replacementChar = String.fromCharCode(
+            Math.floor(Math.random() * 32) + 1072
+          ); // Выбираем случайную букву русского алфавита
+          result =
+            string.substring(0, index) +
+            replacementChar +
+            string.substring(index + 1);
+          break;
+
+        // Удаление символа
+        case 1:
+          result = string.substring(0, index) + string.substring(index + 1);
+          break;
+
+        // Добавление лишнего символа
+        case 2:
+          const extraChar = String.fromCharCode(
+            Math.floor(Math.random() * 32) + 1072
+          );
+          result =
+            string.substring(0, index) + extraChar + string.substring(index);
+          break;
+        default:
+          break;
+      }
+    }
+
+    return result;
+  }
 
   return (
     <>
       <nav className="navbar navbar-light bg-light p-3">
-        <div className="container">
+        <div className="container gap-2">
           <div className="d-flex align-items-center">
             <div className="me-3 fs-5">Reginon:</div>
             <div className="dropdown">
@@ -89,10 +139,20 @@ function App() {
               min={0}
               max={10}
               value={valueSlider}
-              onChange={(e) => handleRangeChange(e)}
+              onChange={(e) => handleSliderChange(e)}
               className="form-control-range me-3"
             />
-            <input value={valueSlider} className="cust_inp_width" type="text" />
+            <input
+              value={valueSlider}
+              onChange={
+                ((e) => setValueSlider(e.target.value),
+                (e) => handleSliderChange(e))
+              }
+              className="cust_inp_width"
+              type="number"
+              min="0"
+              max="1000"
+            />
           </div>
           <div className="d-flex align-items-center">
             <div className="me-3 fs-5">Seed:</div>
@@ -100,8 +160,9 @@ function App() {
           </div>
         </div>
       </nav>
+
       <br />
-      <div className="container">
+      <div className="container table-responsive">
         <table className="table">
           <tbody>
             {users.map((user, index) => {
