3bafa43fb4619c3454913565d57dfe4979af4069
fops777 <huzin.ayratik@gmail.com>
2023-12-26 18:40:15 +0500

    last_fixes

--- src/App.jsx
+++ src/App.jsx
@@ -126,6 +126,30 @@ function App() {
     }
   }
 
+  // При клике на seed random
+  function onSeedRandomClick() {
+    try {
+      setDisabledState(true);
+      let randomVal = Math.floor(Math.random() * 100000);
+      let currVal = randomVal;
+      setSeed(currVal);
+
+      async function getUsers() {
+        let response = axios.post(process.env.REACT_APP_API_URL + "seed", {
+          // let response = axios.post("http://localhost:5555/seed", {
+          seed: currVal,
+        });
+        const data = await response;
+        setUsers([...data.data]);
+        response.then(() => setIsFetching(false));
+        setDisabledState(false);
+      }
+      getUsers();
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }
+
   return (
     <>
       <nav className="navbar navbar-light bg-light p-3">
@@ -141,23 +165,13 @@ function App() {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown link
+                RU-ru
               </a>
 
               <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                 <li>
                   <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
+                    RU-ru
                   </a>
                 </li>
               </ul>
@@ -190,8 +204,28 @@ function App() {
             <input
               value={seed}
               onChange={(e) => onSeedChange(e)}
+              className="me-2"
               type="number"
             />
+            <button
+              onClick={() => onSeedRandomClick()}
+              className="btn btn-outline-dark"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-shuffle"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5"
+                />
+                <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z" />
+              </svg>
+            </button>
           </div>
         </div>
       </nav>
